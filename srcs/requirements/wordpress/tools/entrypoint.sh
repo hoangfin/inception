89623c9f00de9d56d707c43478bb541be567c756
hoangfin <hoang.tran.fin@gmail.com>
2025-03-15 23:44:50 +0200

    Implemented healthcheck for mariadb and wordpress containers

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# Set PHP configuration dynamically
-cat <<EOF > /etc/php83/conf.d/custom.ini
-mysqli.default_host = "${MYSQL_HOST}"
-mysqli.default_user = "${MYSQL_USER}"
-mysqli.default_pw = "${MYSQL_PASSWORD}"
-mysqli.default_db = "${MYSQL_DATABASE}"
-EOF
+set -e
+
+/usr/local/bin/wp-install.sh
 
 echo "Starting PHP-FPM..."
 exec php-fpm83 -F
